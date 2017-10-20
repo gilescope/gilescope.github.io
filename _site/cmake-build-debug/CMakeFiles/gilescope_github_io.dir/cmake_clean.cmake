f143affdd8ba7cffbb7108b97a7bcb0e0b55ce29
Giles Cope <gilescope@gmail.com>
2017-10-20 18:01:25 +0100

    In theory this isn't required.

---
+++ _site/cmake-build-debug/CMakeFiles/gilescope_github_io.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gilescope_github_io.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _site/cmake-build-debug/CMakeFiles/gilescope_github_io.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gilescope_github_io.
+# This may be replaced when dependencies are built.
